06dc926da3e9301382b128b117c65a0f19174f8f
lebantion <58497274+lebantion@users.noreply.github.com>
2022-11-01 18:56:59 -0400

    push userInfo xml

--- app/src/main/java/com/example/buckeyemarket/ui/chatting/ChatActivity.kt
+++ app/src/main/java/com/example/buckeyemarket/ui/chatting/ChatActivity.kt
@@ -1,12 +1,49 @@
 package com.example.buckeyemarket.ui.chatting
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Process
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
 import com.example.buckeyemarket.R
+import com.example.buckeyemarket.databinding.ActivityLoginBinding
+import com.example.buckeyemarket.ui.item.MainActivity
+import com.example.buckeyemarket.ui.item.MyApplication
+import com.google.firebase.auth.FirebaseAuth
+import com.example.buckeyemarket.ui.login.LoginActivity
+import com.example.buckeyemarket.ui.signup.SignUpActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    lateinit var db: FirebaseFirestore
+
+    companion object {
+        private const val TAG = "ChatActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+        setListners()
     }
+
+    private fun loadReceiverInfo() {
+       db.
+
+    }
+
+    private fun setListners() {
+
+    }
+
 }
--- app/src/main/java/com/example/buckeyemarket/ui/userSetting/UserSettingUpActivity.kt
+++ app/src/main/java/com/example/buckeyemarket/ui/userSetting/UserSettingUpActivity.kt
@@ -3,23 +3,14 @@ package com.example.buckeyemarket.ui.userSetting
 import android.content.Intent
 import android.os.Bundle
 import android.os.Process
-import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.OnCompleteListener
 import com.example.buckeyemarket.R
-import com.example.buckeyemarket.databinding.ActivityLoginBinding
 import com.example.buckeyemarket.ui.item.MainActivity
-import com.example.buckeyemarket.ui.item.MyApplication
 import com.google.firebase.auth.FirebaseAuth
 import com.example.buckeyemarket.ui.login.LoginActivity
-import com.example.buckeyemarket.ui.signup.SignUpActivity
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.firestore.FirebaseFirestore
 
 class UserSettingUpActivity : AppCompatActivity(){
@@ -37,7 +28,7 @@ class UserSettingUpActivity : AppCompatActivity(){
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
-        findViewById<View>(R.id.editButton).setOnClickListener(editClickListener)
+        findViewById<View>(R.id.chatButton).setOnClickListener(editClickListener)
         findViewById<View>(R.id.removeAcc).setOnClickListener(removeClickListener)
     }
 
@@ -50,7 +41,7 @@ class UserSettingUpActivity : AppCompatActivity(){
     var editClickListener =
         View.OnClickListener { v ->
             when (v.id) {
-                R.id.editButton -> updateButton()
+                R.id.chatButton -> updateButton()
             }
         }
 
